de613f59d4f8737a9ca8993938fb5e432acb9cc0
Vinh Nguyen <themesexperts1986@gmail.com>
2020-05-05 18:51:45 +0700

    My Way when show result
    - Basic when i don't learn from Angela

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import animals, { useAnimals } from "./data";
 // [cat, dog] = [animals[0], animals[1]];
 // var cat, dog;
 // new way
-const [cat, dog] = animals;
+// const [cat, dog] = animals;
 
-const [animal, makeSound] = useAnimals(cat);
+// const [animal, makeSound] = useAnimals(cat);
 
-console.log(animal);
-makeSound();
+// console.log(animal);
+// makeSound();
 
 // console.log(cat.name);
 // console.log(cat.sound);
@@ -21,44 +21,44 @@ makeSound();
 // console.log(dog.sound);
 
 // import animals from "practice";
-// var tesla, honda, teslaTopSpeed, teslaTopColour, hondaTopSpeed, hondaTopColour;
+var tesla, honda, teslaTopSpeed, teslaTopColour, hondaTopSpeed, hondaTopColour;
 
-// [tesla, honda] = [cars[0], cars[1]];
-// // console.log(tesla.speedStats.topSpeed);
+[tesla, honda] = [cars[0], cars[1]];
+// console.log(tesla.speedStats.topSpeed);
 
-// // console.log(
-// //   tesla.coloursByPopularity[0] + " , " + tesla.coloursByPopularity[1]
-// // );
-
-// [teslaTopSpeed, teslaTopColour] = [
-//   tesla.speedStats.topSpeed,
+// console.log(
 //   tesla.coloursByPopularity[0] + " , " + tesla.coloursByPopularity[1]
-// ];
-
-// [hondaTopSpeed, hondaTopColour] = [
-//   honda.speedStats.topSpeed,
-//   honda.coloursByPopularity[0] + " , " + tesla.coloursByPopularity[1]
-// ];
-
-// ReactDOM.render(
-//   <div>
-//     <table>
-//       <tr>
-//         <th>Brand</th>
-//         <th>Top Speed</th>
-//         <th>Top Colour </th>
-//       </tr>
-//       <tr>
-//         <td>{tesla.model}</td>
-//         <td>{teslaTopSpeed}</td>
-//         <td>{teslaTopColour}</td>
-//       </tr>
-//       <tr>
-//         <td>{honda.model}</td>
-//         <td>{hondaTopSpeed}</td>
-//         <td>{hondaTopColour}</td>
-//       </tr>
-//     </table>
-//   </div>,
-//   document.getElementById("root")
 // );
+
+[teslaTopSpeed, teslaTopColour] = [
+  tesla.speedStats.topSpeed,
+  tesla.coloursByPopularity[0] + " , " + tesla.coloursByPopularity[1]
+];
+
+[hondaTopSpeed, hondaTopColour] = [
+  honda.speedStats.topSpeed,
+  honda.coloursByPopularity[0] + " , " + tesla.coloursByPopularity[1]
+];
+
+ReactDOM.render(
+  <div>
+    <table>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Top Colour </th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </table>
+  </div>,
+  document.getElementById("root")
+);
